0dcaf675018e2d89cd4ce3f68e013f0dc2c36d6c
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2026-05-19 11:36:05 +0200

    App.vue shows userdashboard

--- Frontend/Modulex_Eksamensprojekt/src/App.vue
+++ Frontend/Modulex_Eksamensprojekt/src/App.vue
@@ -1,11 +1,7 @@
-<script setup></script>
+<script setup>
+import UserDashboardView from './views/UserDashboardView.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <UserDashboardView />
 </template>
-
-<style scoped></style>
