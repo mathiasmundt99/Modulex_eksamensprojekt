1f498657e79f977a53ed130a9db58a7ba0bc4c77
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2026-05-19 11:33:40 +0200

    Sidebar: OverviewTab component

---
+++ Frontend/Modulex_Eksamensprojekt/src/components/dashboard/OverviewTab.vue
@@ -0,0 +1,23 @@
+<script setup>
+import WelcomeBanner from './WelcomeBanner.vue'
+import StatsGrid from './StatsGrid.vue'
+import ContinueLearning from './ContinueLearning.vue'
+import RecentActivity from './RecentActivity.vue'
+</script>
+
+<template>
+  <div class="tab-content">
+    <WelcomeBanner />
+    <StatsGrid />
+    <ContinueLearning />
+    <RecentActivity />
+  </div>
+</template>
+
+<style scoped>
+.tab-content {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+</style>
